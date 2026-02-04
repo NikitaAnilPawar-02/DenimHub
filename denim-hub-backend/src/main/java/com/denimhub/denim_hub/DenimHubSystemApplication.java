cc40581447c9f9e635d8ab2117640b85af165f0c
NikitaAnilPawar-02 <pawarnikita0293@gmail.com>
2026-02-04 22:15:01 +0530

    Renamed File

--- denim-hub-backend/src/main/java/com/denimhub/denim_hub/DenimHubSystemApplication.java
+++ denim-hub-backend/src/main/java/com/denimhub/denim_hub/DenimHubSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DenimHubInventoryManagementSystemApplication {
+public class DenimHubSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DenimHubInventoryManagementSystemApplication.class, args);
+		SpringApplication.run(DenimHubSystemApplication.class, args);
 	}
 
 }
