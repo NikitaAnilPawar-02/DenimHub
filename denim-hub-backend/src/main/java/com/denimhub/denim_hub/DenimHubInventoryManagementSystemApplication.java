19f14f4f5ac35f33637e374fd2c0b33b57df8485
NikitaAnilPawar-02 <pawarnikita0293@gmail.com>
2026-02-04 22:07:29 +0530

    Initial Spring Boot project setup

---
+++ denim-hub-backend/src/main/java/com/denimhub/denim_hub/DenimHubInventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.denimhub.denim_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenimHubInventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenimHubInventoryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ denim-hub-backend/src/test/java/com/denimhub/denim_hub/DenimHubInventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denimhub.denim_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenimHubInventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
